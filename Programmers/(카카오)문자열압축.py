73dd8f54a24d44ec29e02627da7408ae76c40c81
MoonSangJin <1128msj@naver.com>
2021-02-23 15:20:02 +0900

    Programmers:kakao

---
+++ Programmers/(카카오)문자열압축.py
@@ -0,0 +1,20 @@
+def solution(s):
+    cand=[]
+    if len(s)==1:
+        return len(s)
+    for size in range(1,len(s)):
+        count=1
+        compressed=''
+        splited=[]
+        for i in range(0,len(s),size):
+            splited.append(s[i:i+size])
+        for j in range(1,len(splited)):
+            if splited[j-1]==splited[j]:
+                count+=1
+            else:
+                compressed+=str(count)+splited[j-1] if count>1 else splited[j-1]
+                count=1
+        compressed+=str(count)+splited[-1] if count>1 else splited[-1]            
+        cand.append(len(compressed))
+        
+    return min(cand)

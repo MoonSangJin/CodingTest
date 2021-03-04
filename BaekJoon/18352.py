05fd34b03e28722099e712db393307a9eded171c
MoonSangJin <1128msj@naver.com>
2021-03-04 17:28:05 +0900

    Book:BFS

---
+++ BaekJoon/18352.py
@@ -0,0 +1,31 @@
+#bfs
+#그래프에서 모든 간선의 비용이 동일하면 BFS 사용한다. 
+from collections import deque
+
+n,m,k,x=map(int,input().split())
+graph=[[] for _ in range(n+1)]
+
+for _ in range(m):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+
+distance=[-1]*(n+1)
+distance[x]=0
+
+q=deque([x]) #이부분 중요, 첫 시작 점을 queue에 넣는다.
+
+while q:
+    now=q.popleft()
+    for next_node in graph[now]:
+        if distance[next_node]==-1: #-1이면 아직 방문안 한 것
+            distance[next_node]=distance[now]+1 #한번만 이동한게 최소길이다.
+            q.append(next_node)
+
+check=False
+for i in range(1,n+1):
+    if distance[i]==k:
+        print(i)
+        check=True
+if check==False:
+    print(-1)                  
+

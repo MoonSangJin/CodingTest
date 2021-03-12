489221b3199cafaa16eb2bd4c858f8c2366d3d00
MoonSangJin <1128msj@naver.com>
2021-03-12 16:54:57 +0900

    type:dfs

---
+++ BaekJoon/2606.py
@@ -0,0 +1,22 @@
+#dfs
+n=int(input())
+pair=int(input())
+graph=[[] for _ in range(n+1)]
+
+for _ in range(pair):
+    a,b=map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+ 
+visited=[False]*(n+1)
+answer=[]
+
+def dfs(graph,v,visited):
+    visited[v]=True
+    answer.append(v)
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph,i,visited)
+
+dfs(graph,1,visited)            
+print(len(answer)-1)

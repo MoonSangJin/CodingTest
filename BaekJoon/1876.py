307a6cd24ba22a2e615060f7bf658d1542c5c846
MoonSangJin <1128msj@naver.com>
2021-03-13 18:27:20 +0900

    type:stack

---
+++ BaekJoon/1876.py
@@ -0,0 +1,23 @@
+no_message=False
+stack=[]
+answer=[]
+cnt=0
+n=int(input())
+for _ in range(n):
+    x=int(input())
+    while cnt<x:
+        cnt+=1
+        stack.append(cnt)
+        answer.append('+')
+    if stack[-1]==x:
+        answer.append('-')
+        stack.pop()
+    else:
+        no_message=True
+        break
+
+if no_message==True:
+    print('NO')
+else:
+    for i in answer:
+        print(i)   

394b190b4e5437a0da6fcaf6fe51fb5a31bdda89
MoonSangJin <1128msj@naver.com>
2021-04-01 19:05:31 +0900

    type:dfs,hash

---
+++ Programmers/네트워크.py
@@ -0,0 +1,21 @@
+#dfs
+def solution(n, computers):
+    answer = 0
+    graph=[[]*n for _ in range(n)]
+    visited=[False for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i!=j and computers[i][j]==1:
+                graph[i].append(j)
+    def dfs(graph,v,visited):
+        visited[v]=True
+        for i in graph[v]:
+            if not visited[i]:
+                dfs(graph,i,visited)
+
+    for i in range(n):
+        if visited[i]==False:
+            dfs(graph,i,visited)
+            answer+=1
+        
+    return answer

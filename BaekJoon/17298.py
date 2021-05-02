dcc9d429700fa8d717811471c68d7120764499c9
MoonSangJin <1128msj@naver.com>
2021-05-02 12:56:48 +0900

    type:stack

--- BaekJoon/17298.py
+++ BaekJoon/17298.py
@@ -24,3 +24,22 @@
 #                 continue
 # for k in result:
 #     print(k, end=' ')
+
+n=int(input())
+nums=list(map(int,input().split()))
+
+stack=[]
+result=[-1 for _ in range(n)]
+
+stack.append(0)
+i=1
+
+while stack and i<n:
+    while stack and nums[stack[-1]]<nums[i]:
+        result[stack[-1]]=nums[i]
+        stack.pop()
+    stack.append(i)
+    i+=1
+
+for i in result:
+    print(i, end=' ')
